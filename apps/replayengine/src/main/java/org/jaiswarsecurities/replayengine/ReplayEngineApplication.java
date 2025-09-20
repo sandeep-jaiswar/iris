7f03b4f4cea7dab4c6d5c1cf825d6dc1ac7394c3
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2025-09-20 15:54:11 +0530

    refractor: Update and reconfigure skeleton of the application

---
+++ apps/replayengine/src/main/java/org/jaiswarsecurities/replayengine/ReplayEngineApplication.java
@@ -0,0 +1,9 @@
+package org.jaiswarsecurities.replayengine;
+
+public class ReplayEngineApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Replay Engine Application Started");
+    }
+
+}
---
+++ apps/replayengine/src/test/java/org/jaiswarsecurities/replayengine/ReplayEngineApplicationTest.java
@@ -0,0 +1,9 @@
+package org.jaiswarsecurities.replayengine;
+
+public class ReplayEngineApplicationTest {
+
+    public static void main(String[] args) {
+        System.out.println("Replay Engine Application Test Started");
+    }
+
+}
